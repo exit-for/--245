d11540d2a0656deb4430fee8d200bd379e7dd19f
exit-for <powel249@gmail.com>
2024-10-14 14:14:46 +0300

    Create 5.py

---
+++ Задание 2/5.py
@@ -0,0 +1,7 @@
+import math
+import numexpr as ne
+x=float(input("Введите переменную x:"))
+y=ne.evaluate(input("Введите переменную y:"))
+z=int(input("Введите переменную z:"))
+s=math.log10(y**(-(math.sqrt(abs(x)))))*(x-(y/2))+math.sin(math.atan(z))**2
+print("s={0:.5f}".format(s))
