94b77bf38e80b855bb874aee3916421f5b47296f
exit-for <powel249@gmail.com>
2024-10-14 14:11:44 +0300

    1.py

---
+++ Задание 2/1.py
@@ -0,0 +1,7 @@
+import math
+import numexpr as ne
+x=float(input("Введите переменную x:"))
+y=float(input("Введите переменную y:"))
+z=ne.evaluate(input("Введите переменную z:"))
+s=(2 * math.cos(x-2/3))/(1/2 + math.sin(y)**2)*(1 + (( z **2 )/( 3 - z **2 / 5 )))
+print("s={0:.5f}".format(s))
