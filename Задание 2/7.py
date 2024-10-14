803258617e687b04a2274e7f0de3e1a30f0e73b0
exit-for <powel249@gmail.com>
2024-10-14 14:15:11 +0300

    Create 7.py

---
+++ Задание 2/7.py
@@ -0,0 +1,7 @@
+import math
+import numexpr as ne
+x=float(input("Введите переменную x:"))
+y=float(input("Введите переменную y:"))
+z=ne.evaluate(input("Введите переменную z:"))
+s=5*math.atan(x)-1/4*math.acos(x)*((x+3*abs(x-y)+x**2)/(abs(x-y)*z+x**2))
+print("s={0:.3f}".format(s))
