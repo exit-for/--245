808cf4931ced7f192bceee866249dcc210870310
exit-for <powel249@gmail.com>
2024-10-14 14:16:15 +0300

    Create 9.py

---
+++ Задание 2/9.py
@@ -0,0 +1,7 @@
+import math
+import numexpr as ne
+x=ne.evaluate(input("Введите переменную x:"))
+y=float(input("Введите переменную y:"))
+z=ne.evaluate(input("Введите переменную z:"))
+s=abs(x**(y/x)-math.pow(y/x,1/3))+(y-x)*((math.cos(y)-((z)/(y-x)))/(1+(y-x)**2))
+print("s={0:.5f}".format(s))
