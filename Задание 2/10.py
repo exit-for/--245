0b2411ba93acdbc693032b1cbdbb4ad8c10d6bda
exit-for <powel249@gmail.com>
2024-10-28 16:21:42 +0300

    10.py

--- Задание 2/10.py
+++ Задание 2/10.py
@@ -1,7 +1,8 @@
 import math
-import numexpr as ne
-x=ne.evaluate(input("Введите переменную x:"))
-y=ne.evaluate(input("Введите переменную y:"))
-z=float(input("Введите переменную z:"))
-s=2**(-x)*math.sqrt(x+(math.pow(abs(y),1/4)))*(math.pow(math.exp(x-1/math.sin(z)),1/3))
-print("s={0:.5f}".format(s))
+
+x=eval(input('введите переменную х  :'))
+y=eval(input('введите переменную y  :'))
+z=float(input('введите переменную z  :'))
+s=(2**(-x))*((x+abs(y)**(1/4))**(1/2))*(math.pow(math.exp(x-1/math.sin(z)),1/3))
+
+print(round(s,5))
