80047c10efc5b243880c241f4138aa0cf0b6e44d
exit-for <powel249@gmail.com>
2024-10-28 16:25:15 +0300

    14.py

--- Задание 2/14.py
+++ Задание 2/14.py
@@ -1,7 +1,9 @@
 import math
-import numexpr as ne
-x=ne.evaluate(input("Введите переменную x:"))
-y=float(input("Введите переменную y:"))
-z=ne.evaluate(input("Введите переменную z:"))
-s=((y**(x+1))/(math.pow(abs(y-2),1/3)+3))+((x+y/2)/(2*abs(x+y)))*(x+1)**(-1/math.sin(z))
-print("s={0:.4f}".format(s))
+
+
+x=eval(input('введите переменную х  :'))
+y=float(input('введите переменную y  :'))
+z=eval(input('введите переменную z  :'))
+s=((y**(x+1))/((abs(y-2))**(1/3)+3))+((x+(y/2))/(2*(abs(x+y))))*((x+1)**((-1)/math.sin(z)))
+
+print(round(s,4))
