c92dd08a3905aad7ccce31380aa3642293d99601
exit-for <powel249@gmail.com>
2024-10-28 16:16:25 +0300

    8.py

--- Задание 2/8.py
+++ Задание 2/8.py
@@ -1,9 +1,8 @@
 import math
-import numexpr as ne
 
-x=ne.evaluate(input("Введите переменную x  :"))
-y=float(input("Введите переменную y  :"))
-z=float(input("Введите переменную z  :"))
-s=((math.exp(abs(x-y))*abs(x-y)**(x+y))/(math.atan(x)+math.atan(z)))+math.pow(x**6+math.log10(y)**2,1/3)
+x=eval(input('введите переменную х  :'))
+y=float(input('введите переменную y  :'))
+z=float(input('введите переменную z  :'))
+s=((math.exp(abs(x-y))*abs(x-y)**(x+y))/(math.atan(x)+math.atan(z)))+((x**6+math.log(y,math.e)**2)**(1/3))
 
-print("s={0:.4f}".format(s))
+print(round(s,4))
