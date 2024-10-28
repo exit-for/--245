a09bef5d23288740f16441ca48e3c6064333612e
exit-for <powel249@gmail.com>
2024-10-28 16:24:27 +0300

    12.py

--- Задание 2/12.py
+++ Задание 2/12.py
@@ -1,7 +1,8 @@
 import math
-import numexpr as ne
-x=float(input("Введите переменную x:"))
-y=float(input("Введите переменную y:"))
-z=ne.evaluate(input("Введите переменную z:"))
-s=2**(y**x)+(3**x)**y-((y*(math.atan(z)-1/3))/(abs(x)+((1)/(y**2+1))))
-print("s={0:.5f}".format(s))
+
+x=float(input('введите переменную х  :'))
+y=float(input('введите переменную y  :'))
+z=eval(input('введите переменную z  :'))
+s=(2**(y**x))+((3**x)**y)-((y*(math.atan(z)-(1/3)))/(abs(x)+(1/((y**2)+1))))
+
+print(round(s,5))
