3f569efcd8915ca1d628f2837a1f3e8ea7e3c865
exit-for <powel249@gmail.com>
2024-10-28 16:09:31 +0300

    2.py

--- Задание 2/2.py
+++ Задание 2/2.py
@@ -1,9 +1,8 @@
 import math
-import numexpr as ne
 
-x=float(input("Введите переменную x  :"))
-y=ne.evaluate(input("Введите переменную y  :"))
-z=ne.evaluate(input("Введите переменную z  :"))
-s=((math.pow(9+(x-y)**2,1/3))/(x**2+y**2+2))-math.exp(abs(x-y))*math.tan(z)**3
+x=float(input('введите переменную х  :'))
+y=eval(input('введите переменную y  :'))
+z=eval(input('введите переменную z:  '))
+s=(((9+(x-y)**2)**(1/3))/(x**2+y**2+2))-(math.exp(abs(x-y)))*math.tan(z)**3
 
-print("s={0:.5f}".format(s))
+print(round(s,5))
