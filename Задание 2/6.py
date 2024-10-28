66339484da46925c74cb6231ef02dcd341308f0d
exit-for <powel249@gmail.com>
2024-10-28 16:14:56 +0300

    6.py

--- Задание 2/6.py
+++ Задание 2/6.py
@@ -1,9 +1,8 @@
 import math
-import numexpr as ne
 
-x=ne.evaluate(input("Введите переменную x  :"))
-y=float(input("Введите переменную y  :"))
-z=float(input("Введите переменную z  :"))
-s=math.sqrt(10*(math.pow(x,1/3)+x**(y+2)))*(math.asin(z)**2-abs(x-y))
+x=eval(input('введите переменную х  :'))
+y=float(input('введите переменную y  :'))
+z=float(input('введите переменную z  :'))
+s=(math.sqrt((x**(1/3))+(x**(y+2)))*(10**(1/2)))*(math.asin(z)**2-abs(x-y))
 
-print("s={0:.4f}".format(s))
+print(round(s,4))
