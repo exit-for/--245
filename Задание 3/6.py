459bf5ed3bc052b94bb0c1a769a5b9591e13c762
exit-for <powel249@gmail.com>
2024-10-28 16:27:03 +0300

    6.py

--- Задание 3/6.py
+++ Задание 3/6.py
@@ -2,6 +2,7 @@ a1=int(input('номер столбца 1'))
 a2=int(input('номер клетки 1'))
 b1=int(input('номер столбца 2'))
 b2=int(input('номер клетки 2'))
+
 def f(a1,a2,b1,b2):
     return(a1+b1)%2==(a2+b2)%2
 if f(a1,a2,b1,b2):
