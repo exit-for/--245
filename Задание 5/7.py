3ec77a115d6d12712485fee2a8cff7ad25eae8e9
exit-for <powel249@gmail.com>
2024-10-28 16:32:39 +0300

    7.py

--- Задание 5/7.py
+++ Задание 5/7.py
@@ -1,5 +1,6 @@
 c = 0  
 pn = None  
+
 while True:  
     n = int(input("Введите натуральное число (0 для завершения): ")) 
     if n == 0:  
@@ -7,4 +8,5 @@ while True:
     if pn is not None and n > pn:
         c += 1  
     pn = n
+    
 print(f"Количество элементов, больших предыдущего: {c}")
