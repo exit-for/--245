f250fa5d03679bf1ea91f9070bc2bc84c2c02636
exit-for <powel249@gmail.com>
2024-10-28 14:05:16 +0300

    9.py

---
+++ Задание 4/9.py
@@ -0,0 +1,13 @@
+N=int(input())
+def f(n):
+    a,b=0,1
+    summa=0
+    for i in range(n):
+        summa+=a
+        a,b=b,a+b
+    return summa
+if N>0:
+    r=f(N)
+    print(f'сумма {N} чисел Фибоначчи равна: {r}')
+else:
+    print('введите число больше 0')
