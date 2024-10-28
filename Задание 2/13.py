3d97f062d59cb23f2c5173156f9fbdea0835895f
exit-for <powel249@gmail.com>
2024-10-28 16:24:52 +0300

    13.py

--- Задание 2/13.py
+++ Задание 2/13.py
@@ -1,7 +1,8 @@
 import math
-import numexpr as ne
-x=float(input("Введите переменную x:"))
-y=ne.evaluate(input("Введите переменную y:"))
-z=ne.evaluate(input("Введите переменную z:"))
-s=(math.pow(y+math.pow(x-1,1/3),1/4)/(abs(x-y)*(math.sin(z)**2+math.tan(z))))
-print("s={0:.6f}".format(s))
+
+x=float(input('введите переменную х  :'))
+y=eval(input('введите переменную y  :'))
+z=eval(input('введите переменную z  :'))
+s=((y+((x-1)**(1/3)))**(1/4))/(abs(x-y)*(math.sin(z)**2+math.tan(z)))
+
+print(round(s,6))
